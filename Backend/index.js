388b209b5e93c733829ddce561a11fe8dce3638a
pavankumar0039 <thumatipavanchowdary@gmail.com>
2024-11-30 17:24:19 +0530

    modified:   Backend/index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,10 +10,11 @@ const app = express();
 //middlewares that supports backend
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors( {
+    origin: ['http://localhost:3000', 'https://grocerywebapp-1.onrender.com'], // Allow both localhost and your deployed frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow necessary HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+  }));
 app.use(cookieParser())
 
 //userdefined modules
