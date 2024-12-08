98131d596e23a3cb603babab8111e28f903e13c8
pavankumar0039 <thumatipavanchowdary@gmail.com>
2024-12-08 09:20:56 +0530

    modified:   Backend/Controllers/UserControler/Removefromcart.js

--- Backend/Controllers/UserControler/Removefromcart.js
+++ Backend/Controllers/UserControler/Removefromcart.js
@@ -1,8 +1,9 @@
 const mongoose=require('mongoose')
 const Cartschema=require('../../models/Usercart')
 async function Removefromcart(req,res){
-     let {email}=req.body.email
+     let email=req.body.email
      let product_id=req.body.productid;
+     console.log(product_id)
     let collectionname=email+"cart";
     console.log(collectionname);
     if (mongoose.models[collectionname]) {
@@ -11,7 +12,7 @@ async function Removefromcart(req,res){
         cartmodel = mongoose.model(collectionname, Cartschema);
     }
     try {
-        let cartremovedproduct=await cartmodel.findOneAndDelete({ product_id })
+        let cartremovedproduct=await cartmodel.findOneAndDelete( product_id )
         res.status(200).json({
             message:"product delete to cart",
             data:cartremovedproduct,
